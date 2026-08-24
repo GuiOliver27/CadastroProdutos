57c774006695ca11d251cc4b5a5dd3e4d1819d6f
“Guilherme <guilherme_oliveira27@outlook.com>
2026-08-23 21:30:31 -0400

    Feat: Validação de dados e regras de negócio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using CadastroProdutos.Database;
 using CadastroProdutos.Services;
 using Microsoft.EntityFrameworkCore;
@@ -115,7 +116,14 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 public class Produto
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+    [StringLength(100, ErrorMessage = "O nome do produto não pode exceder 100 caracteres.")]
     public string Nome { get; set; }
+
+    [Range(0.01, Double.MaxValue, ErrorMessage = "O preço do produto deve ser maior que zero.")]
     public decimal Preco { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "O estoque do produto não pode ser negativo.")]
     public int Estoque { get; set; }
 }
